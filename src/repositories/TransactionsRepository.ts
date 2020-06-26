6050146402b5f84adfb2cfd4a9891341be6b0c04
nortongoncalves <goncalvespnorton@gmail.com>
2020-06-26 15:24:42 -0300

    finalizando tarefa

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface createTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const sumAllIncomes = this.transactions
+      .filter(({ type }) => type === 'income')
+      .reduce((sum, { value }) => {
+        return sum + value;
+      }, 0);
+    const sumAllOutcome = this.transactions
+      .filter(({ type }) => type === 'outcome')
+      .reduce((sum, { value }) => {
+        return sum + value;
+      }, 0);
+    const sumTotal = sumAllIncomes - sumAllOutcome;
+    const balance = {
+      income: sumAllIncomes,
+      outcome: sumAllOutcome,
+      total: sumTotal,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: createTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    const balance = this.getBalance();
+    const haveBalance = balance.total >= value;
+    if (!haveBalance && type === 'outcome')
+      throw Error('insufficient balance to complete the transaction');
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
